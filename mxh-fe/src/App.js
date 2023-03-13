6e05c2b27f566c5c8f2a2a8101e793cc56ee354a
dai <1lanyeue@gmail.com>
2023-03-13 14:50:29 +0700

    firt

---
+++ mxh-fe/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+        <Route path="/home" element={<Home/>}/>
+    </Routes>
+  );
+}
+
+export default App;
