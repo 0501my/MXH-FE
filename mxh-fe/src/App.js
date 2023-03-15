9872d6d4d286aebd5c6201b19b4199b947341310
dai <1lanyeue@gmail.com>
2023-03-15 15:24:34 +0700

    xoa editProfile va logginGG

--- mxh-fe/src/App.js
+++ mxh-fe/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import React from "react";
 import {Route, Routes} from "react-router-dom";
@@ -7,21 +6,18 @@ import Home from "./pages/Home";
 import ShowHome from "./pages/ShowHome";
 import PersonalPage from "./pages/PersonalPage";
 import Register from "./pages/Register";
-import {GoogleLoginButton} from "react-social-login-buttons";
-import {LoginSocialGoogle} from "reactjs-social-login";
-import LoginGoogle from "./pages/LoginGoogle";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login/>}/>
-        <Route path="/registers" element={<Register/>}/>
-        <Route path="/home" element={<Home/>}>
-            <Route path={""} element={<ShowHome/>}/>
-            <Route path={"personalPage"} element={<PersonalPage/>}/>
-        </Route>
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<Login/>}/>
+            <Route path="/registers" element={<Register/>}/>
+            <Route path="/home" element={<Home/>}>
+                <Route path={""} element={<ShowHome/>}/>
+                <Route path={"personalPage"} element={<PersonalPage/>}/>
+            </Route>
+        </Routes>
+    )
 }
 
 export default App;
