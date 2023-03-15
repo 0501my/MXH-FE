c8e4af5f92949e43ce174371cae1c3b431d9fafb
dai <1lanyeue@gmail.com>
2023-03-15 10:02:09 +0700

    sau khi mer FEedit

--- mxh-fe/src/App.js
+++ mxh-fe/src/App.js
@@ -5,11 +5,8 @@ import {Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import ShowHome from "./pages/ShowHome";
-import AddPost from "./pages/AddPost";
 import PersonalPage from "./pages/PersonalPage";
-import EditPost from "./pages/EditPost";
 import Register from "./pages/Register";
-import Header from "./component/Header";
 
 function App() {
   return (
@@ -18,8 +15,6 @@ function App() {
         <Route path="/registers" element={<Register/>}/>
         <Route path="/home" element={<Home/>}>
             <Route path={""} element={<ShowHome/>}/>
-            <Route path={"addPost"} element={<AddPost/>}/>
-            <Route path="posts/:id" element={<EditPost/>}/>
             <Route path={"personalPage"} element={<PersonalPage/>}/>
         </Route>
     </Routes>
