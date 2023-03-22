2c5cd219fedd865a4be7e0f2a7da0742d36c298f
namngo93 <ngonam199380@gmail.com>
2023-03-22 11:03:15 +0700

    merge

--- mxh-fe/src/pages/MyAbout.js
+++ mxh-fe/src/pages/MyAbout.js
@@ -67,8 +67,8 @@ const MyAbout = () => {
                                                 </button>
                                                 <ul className="dropdown-menu dropdown-menu-end"
                                                     aria-labelledby="profileAction2">
-                                                    <li><Link className="dropdown-item" to={`/settings`}> <i
-                                                        className="bi bi-lock fa-fw pe-2"></i>Change profile</Link></li>
+                                                    <li><Link className="dropdown-item" to={`/home/settings`}> <i
+                                                        className="bi bi-lock fa-fw pe-2"></i>Setting Account</Link></li>
                                                 </ul>
                                             </div>
                                         </div>
