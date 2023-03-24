9fe7592ec961c7d1afcb89f512a29ded3d8b4368
trung110152 <trung110152@gmail.com>
2023-03-24 17:18:10 +0700

    test sau pull

--- mxh-fe/src/pages/MyTimeLine.js
+++ mxh-fe/src/pages/MyTimeLine.js
@@ -73,7 +73,7 @@ const MyTimeLine = () => {
 
                                                 <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="profileAction2">
 
-                                                    <li><Link className="dropdown-item" to={"/home/settings"}> <i className="bi bi-lock fa-fw pe-2"></i>Change password</Link></li>
+                                                    <li><Link className="dropdown-item" to={"/home/settings"}> <i className="bi bi-lock fa-fw pe-2"></i>Change profile</Link></li>
 
                                                 </ul>
                                             </div>
